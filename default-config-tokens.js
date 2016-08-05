fb23623a9029c7340ae2782aa130c62ca440bae0
ludoo0d0a <ludovic.valente@gmail.com>
2016-08-05 08:08:07 +0000

    add template for config

---
+++ default-config-tokens.js
@@ -0,0 +1,6 @@
+module.exports = {
+    
+    "tokensecret": "123456789ABCDEFGHIJKLM=",
+    "token": "123456789/ABCDEFGHIJKLM="
+
+};
---
+++ default-config2.js
@@ -0,0 +1,7 @@
+module.exports = {
+    consumerKey: '123456789-ABCD-123456789-ABCD',
+    consumerSecret: '123456789-ABCD-123456789-ABCD',
+    callbackURL: 'https://gc.myserver.com/auth/geocaching/callback',
+    
+    live: false
+};
